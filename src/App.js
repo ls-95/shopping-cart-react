4cf989adfc5bf6775a43a8bd4002a3eb75ef1601
ls-95 <143622355+ls-95@users.noreply.github.com>
2025-09-03 10:00:32 +0200

    Move item component from app to home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import NavBar from "./components/NavBar.js";
-import Item from "./components/Item.js";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Item />
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,5 @@
+import Item from "../components/Item";
+
 export default function Home() {
-  return <p>Home</p>;
+  return <Item />;
 }
